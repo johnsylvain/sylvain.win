fd5d0d3e66600a524a657fbf8cddac7ed105ece6
John Sylvain <john.n.sylvain@gmail.com>
2018-02-24 18:49:35 -0600

    move webtask function

--- webtask/wakatime.js
+++ webtask/wakatime.js
@@ -19,8 +19,8 @@ const getTime = new Promise((resolve, reject) => {
   fetch('https://wakatime.com/share/@johnsylvain/75912b47-4ab5-46ce-babd-b1883a7e211b.json')
     .then(res => res.json())
     .then(json => {
-        const seconds = json.data.reduce((acc, cur) => acc + cur.grand_total.total_seconds, 0)
-        return resolve(convert(seconds))
+      const seconds = json.data.reduce((acc, cur) => acc + cur.grand_total.total_seconds, 0)
+      return resolve(convert(seconds))
     })
 })
 
@@ -28,13 +28,14 @@ const getLanguages = new Promise((resolve, reject) => {
   fetch('https://wakatime.com/share/@johnsylvain/69c54bff-19e2-41a4-970d-5da9e24427d6.json')
     .then(res => res.json())
     .then(json => {
-        return resolve(json.data.filter(i => i.name !== 'Other').splice(0, 3))
+      return resolve(json.data.filter(i => i.name !== 'Other').splice(0, 3))
     })
 })
+
 /**
 * @param context {WebtaskContext}
 */
-module.exports = function(context, cb) {
+module.exports = function (context, cb) {
   Promise.all([getTime, getLanguages])
     .then(result => {
       cb(null, {
